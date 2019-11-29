0aa890b91180ce228d45e0b1789dc3ce310487f8
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-29 14:30:13 +0000

    Bishop moves

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -6,11 +7,29 @@ namespace Chessington.GameEngine.Pieces
     public class Bishop : Piece
     {
         public Bishop(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableSquares = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            var BoardSize = GameSettings.BoardSize;
+            var portelanSquareRow = currentSquare.Row;
+            var portelanSquareCol = currentSquare.Col;
+
+            for (var i = 1; i <= BoardSize; i++)
+            {
+                availableSquares.Add(Square.At(currentSquare.Row + i, currentSquare.Col + i));
+                availableSquares.Add(Square.At(currentSquare.Row + i, currentSquare.Col - i));
+                availableSquares.Add(Square.At(currentSquare.Row - i, currentSquare.Col + i));
+                availableSquares.Add(Square.At(currentSquare.Row - i, currentSquare.Col - i));
+            }
+
+            availableSquares.RemoveAll(s => s.Row > 7 || s.Row < 0 || s.Col > 7 || s.Col < 0);
+
+            return availableSquares;
         }
     }
 }
